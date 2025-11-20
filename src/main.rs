53237a3d12eee48961eb001b248f992fff4bcd25
Sarah <sarah.theoulle@etu.umontpellier.fr>
2025-11-20 13:51:23 +0100

    chore: implement OpenTelemetry metrics and tracing in app module

--- src/main.rs
+++ src/main.rs
@@ -1,153 +1,19 @@
 use clap::Parser;
 use dotenv::dotenv;
-use std::env;
 
 
 use crate::config::Config;
-
 mod config;
 mod error;
 mod http;
+mod app;
 
-use opentelemetry::{global, trace::TracerProvider as _, KeyValue};
-use opentelemetry_sdk::{
-    metrics::{MeterProviderBuilder, PeriodicReader, SdkMeterProvider},
-    trace::{RandomIdGenerator, Sampler, SdkTracerProvider},
-    Resource,
-};
-use opentelemetry_semantic_conventions::{
-    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, SERVICE_VERSION},
-    SCHEMA_URL,
-};
-use tracing_core::Level;
-use tracing_opentelemetry::{MetricsLayer, OpenTelemetryLayer};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-fn resource() -> Resource {
-    Resource::builder()
-        .with_service_name(env!("CARGO_PKG_NAME"))
-        .with_schema_url(
-            [
-                KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
-                KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
-            ],
-            SCHEMA_URL,
-        )
-        .build()
-}
-
-fn init_meter_provider() -> SdkMeterProvider {
-    let exporter = opentelemetry_otlp::MetricExporter::builder()
-        .with_tonic()
-        .with_temporality(opentelemetry_sdk::metrics::Temporality::default())
-        .build()
-        .unwrap();
-
-    let reader = PeriodicReader::builder(exporter)
-        .with_interval(std::time::Duration::from_secs(30))
-        .build();
-
-    let stdout_reader =
-        PeriodicReader::builder(opentelemetry_stdout::MetricExporter::default()).build();
-
-    let meter_provider = MeterProviderBuilder::default()
-        .with_resource(resource())
-        .with_reader(reader)
-        .with_reader(stdout_reader)
-        .build();
-
-    global::set_meter_provider(meter_provider.clone());
-
-    meter_provider
-}
-
-fn init_tracer_provider() -> SdkTracerProvider {
-    let exporter = opentelemetry_otlp::SpanExporter::builder()
-        .with_tonic()
-        .build()
-        .unwrap();
-
-    SdkTracerProvider::builder()
-        .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
-            1.0,
-        ))))
-        .with_id_generator(RandomIdGenerator::default())
-        .with_resource(resource())
-        .with_batch_exporter(exporter)
-        .build()
-}
-
-fn init_tracing_subscriber() -> OtelGuard {
-    let tracer_provider = init_tracer_provider();
-    let meter_provider = init_meter_provider();
-
-    let tracer = tracer_provider.tracer("tracing-otel-subscriber");
-
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::filter::LevelFilter::from_level(
-            Level::INFO,
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .with(MetricsLayer::new(meter_provider.clone()))
-        .with(OpenTelemetryLayer::new(tracer))
-        .init();
-
-    OtelGuard {
-        tracer_provider,
-        meter_provider,
-    }
-}
-
-struct OtelGuard {
-    tracer_provider: SdkTracerProvider,
-    meter_provider: SdkMeterProvider,
-}
-
-impl OtelGuard {
-    /// Shutdown providers and flush any buffered telemetry.
-    pub async fn shutdown(self) {
-        let tracer_provider = self.tracer_provider;
-        let meter_provider = self.meter_provider;
-
-        let _ = tokio::task::spawn_blocking(move || {
-            if let Err(err) = tracer_provider.shutdown() {
-                eprintln!("tracer shutdown error: {err:?}");
-            }
-            if let Err(err) = meter_provider.shutdown() {
-                eprintln!("meter shutdown error: {err:?}");
-            }
-        })
-        .await;
-    }
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     let config = Config::parse();
-    let guard = init_tracing_subscriber();
 
-    foo().await;
-
-    let _ = crate::http::serve(config)
-        .await
-        .inspect_err(|e| eprintln!("{}", e));
-
-    guard.shutdown().await;
-
-    Ok(())
-
-
+    app::run(config).await
 }
 
-#[tracing::instrument]
-async fn foo() {
-    tracing::info!(
-        monotonic_counter.foo = 1_u64,
-        key_1 = "bar",
-        key_2 = 10,
-        "handle foo",
-    );
-
-    tracing::info!(histogram.baz = 10, "histogram example",);
-}
