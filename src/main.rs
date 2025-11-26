9cbc3a9ce2e2b71630c5d0238478e1d09b18ec22
Sarah THEOULLE <106761192+stheoulle@users.noreply.github.com>
2025-11-26 08:01:12 +0100

    chore: add tracing to the service (#8)
    * chore: update dependencies and add tracing setup
    
    * chore: add OpenTelemetry collector configuration and update docker-compose
    
    * chore: update OpenTelemetry configuration and dependencies, add Prometheus support
    
    * chore: update OpenTelemetry setup with new dependencies and configuration
    
    * chore: update environment configuration
    
    * chore: clean up comments and improve shutdown handling in OtelGuard
    
    * chore: implement OpenTelemetry metrics and tracing in app module
    
    * chore: add telemetry module with OpenTelemetry setup and logging instructions to CONTRIBUTING.md
    
    * chore: pin otel-collector image version
    
    * chore: enhance error handling with TelemetryError
    
    * chore: refactor telemetry initialization and update config handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use clap::Parser;
 use dotenv::dotenv;
 
-use crate::config::Config;
 
+use crate::config::Config;
 mod config;
 mod error;
 mod http;
+mod telemetry;
+
+use crate::error::CoreError;
+
+
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), CoreError>{
     dotenv().ok();
     let config = Config::parse();
-    let _ = crate::http::serve(config)
+
+    let guard = telemetry::init(&config)
+        .map_err(|e| CoreError::HttpServer(format!("Telemetry error: {}", e)))?;
+
+    let _ = crate::http::serve(&config)
         .await
         .inspect_err(|e| eprintln!("{}", e));
+
+    guard.shutdown().await;
+
+    Ok(())
 }
+
