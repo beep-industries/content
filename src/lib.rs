d77f8183ee7c3dec18690d3122048784c2f8c0b7
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2026-01-21 10:41:28 +0100

    feat: add guard

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,14 @@ pub async fn app(config: Arc<Config>, time: RealTime) -> Result<(), CoreError> {
                     crate::guards::FileType::ImageGIF,
                 ]),
             )
+            .add(
+                "server_picture",
+                crate::guards::Guard::new(vec![
+                    crate::guards::FileType::ImagePNG,
+                    crate::guards::FileType::ImageJPEG,
+                    crate::guards::FileType::ImageGIF,
+                ]),
+            )
             .build(),
     );
     let app_state: AppState =
